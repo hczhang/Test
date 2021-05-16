f30c4b3045d71aebe3825cafde5fd9f787c0a4ee
hczhang <hancai.zhang@outlook.com>
2021-05-16 22:44:38 +0800

    hello world

---
+++ Hello/src/com/choi/example/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.choi.example;
+
+/**
+ * @aouthor choi
+ * @date 2021/5/16-22:43
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
