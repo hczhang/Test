3885e584cdddb08233d66d4750d375415f0bb4b8
hczhang <hancai.zhang@outlook.com>
2021-05-16 23:01:20 +0800

    hello github

---
+++ Hello/src/com/choi/example/HelloGitHub.java
@@ -0,0 +1,11 @@
+package com.choi.example;
+
+/**
+ * @aouthor choi
+ * @date 2021/5/16-23:00
+ */
+public class HelloGitHub {
+    public static void main(String[] args) {
+        System.out.println("hello github");
+    }
+}
